f4011d43c43f7554ddca599e9fb680df5de5d5a1
Jeff Sabin <jsabin@proofpoint.com>
2014-04-04 14:34:04 -0600

    Reader is now a thread that uploads metrics after 10 seconds.

--- src/main/python/kairos/reader.py
+++ src/main/python/kairos/reader.py
@@ -1,61 +1,74 @@
 #!/usr/bin/python
 import time
+import threading
 
 
-def reader(uploader, pipe):
-    metricFile = "./pendingMetrics.json"
-    out = open(metricFile, 'w')
-    out.write('[')
+class Reader(threading.Thread):
+    def __init__(self, uploader, pipe):
+        self.uploader = uploader
+        self.pipe = pipe
+        self.done = False
 
-    #print 'about to open'
-    rp = open(pipe, 'r')  # Blocks here until the first message is sent
+        threading.Thread.__init__(self, target = self.reader)
+        self.start()
 
-    metricCount = 0
-    done = False
-    while not done:
-        #print 'About to read' 
-        response = rp.readline()
-        if not response:
-            time.sleep(0.05)
-        else:
-            metric = response.strip().split(' ')
-            
-            if (len(metric) < 3):
-            	print 'Bad metric '+response
-            	continue
-            
-            print metric
+    def stop(self):
+        self.done = True
 
-            # todo use Object to encode json
-            if metricCount > 0:
-                out.write(',')
-            out.write('{')
-            out.write('"name": "%s",' % metric[0])
-            out.write('"timestamp": %s,' % metric[1])
-            out.write('"value": %s,' % metric[2])
 
-            out.write('"tags":{')
-            tagCount = 0
-            for tagString in metric[3:]:
-                if tagCount > 0:
+    def reader(self):
+        metricFile = "./pendingMetrics.json"
+        out = open(metricFile, 'w')
+        out.write('[')
+
+        rp = open(self.pipe, 'r')  # Blocks here until the first message is sent
+
+        metricCount = 0
+        startTime = time.time()
+        while not self.done:
+            # print "Running"
+            response = rp.readline()
+            if not response:
+                time.sleep(0.05)
+            else:
+                metric = response.strip().split(' ')
+
+                if (len(metric) < 3):
+                    print 'Bad metric ' + response
+                    continue
+
+                print metric
+
+                # todo use Object to encode json
+                if metricCount > 0:
                     out.write(',')
-                tag = tagString.split('=')
-                out.write('"%s": "%s"' % (tag[0], tag[1]))
-                tagCount +=1
-            out.write('}')
+                out.write('{')
+                out.write('"name": "%s",' % metric[0])
+                out.write('"timestamp": %s,' % metric[1])
+                out.write('"value": %s,' % metric[2])
 
-            out.write('}')
+                out.write('"tags":{')
+                tagCount = 0
+                for tagString in metric[3:]:
+                    if tagCount > 0:
+                        out.write(',')
+                    tag = tagString.split('=')
+                    out.write('"%s": "%s"' % (tag[0], tag[1]))
+                    tagCount += 1
+                out.write('}')
 
-            out.flush()
-            metricCount += 1
+                out.write('}')
 
-        if metricCount > 1:
-            metricCount = 0
-            out.write(']')
-            out.close()
-            if uploader.upload(metricFile, False):
-                out = open(metricFile, 'w')
-                out.write('[')
-            # todo retry if failed?
+                out.flush()
+                metricCount += 1
 
-#rp.close()
+            if time.time() > (startTime + 10) and metricCount > 0:
+                print "sending metric"
+                metricCount = 0
+                out.write(']')
+                out.close()
+                if self.uploader.upload(metricFile, False):
+                    out = open(metricFile, 'w')
+                    out.write('[')
+                    # todo retry if failed?
+                startTime = time.time()
--- src/main/python/start.py
+++ src/main/python/start.py
@@ -1,17 +1,26 @@
+#!/usr/bin/python
+
 import sys
 import os
 import os.path
 import ConfigParser
-from kairos import redirector
+import signal
 from kairos.uploader import Uploader
-from kairos import reader
+from kairos.reader import Reader
 
-print "Sarting up"
+
+def handler(signum, frame):
+    global reader
+    print "Exiting"
+    reader.stop()
+
+
+print "Starting up"
 
 if (len(sys.argv) != 2):
-	print 'You must provide a path to the redirector.ini!'
-	sys.exit(1)
-	
+    print 'You must provide a path to the redirector.ini!'
+    sys.exit(1)
+
 configPath = sys.argv[1]
 
 config = ConfigParser.ConfigParser()
@@ -25,19 +34,20 @@ print fifoPath
 
 #create named pipe
 if (os.path.exists(fifoPath)):
-	os.remove(fifoPath)
+    os.remove(fifoPath)
 
 os.mkfifo(fifoPath)
 os.chmod(fifoPath, 0666)
 
-#register signal handler
-
-
 #callinto pipe reader
 upload = Uploader(kairosUrl)
 
+reader = Reader(upload, fifoPath)
 
-reader.reader(upload, fifoPath)
+#register signal handler
+signal.signal(signal.SIGINT, handler)
+signal.pause()
+
+reader.join()
 os.remove(fifoPath)
-
 print "Exiting"
